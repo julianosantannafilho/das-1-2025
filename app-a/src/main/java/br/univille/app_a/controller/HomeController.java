3df2a8f81c6b34e17d39f3061b90f740e7053072
julianosantannafilho <168118959+julianosantannafilho@users.noreply.github.com>
2025-06-26 00:15:30 +0000

    microsservices

---
+++ app-a/src/main/java/br/univille/app_a/controller/HomeController.java
@@ -0,0 +1,35 @@
+package br.univille.app_a.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.domain.HttpExtension;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+
+// dispose = desconectar os serviços
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    //  ******** APPLICAÇÃO A  ************
+
+    @GetMapping()
+    public ResponseEntity index() {
+        System.out.println("Hello from App A");
+        try(DaprClient daprClient = new DaprClientBuilder().build()){
+
+            daprClient.invokeMethod("app-b", "/api", null, HttpExtension.GET).block();
+
+        } catch (Exception e){
+            //TODO: handle exceptions
+        }
+
+        return ResponseEntity.ok().body("Hello from App A");
+    }
+
+}
