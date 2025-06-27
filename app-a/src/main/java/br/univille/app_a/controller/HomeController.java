e920b7f5222f5acd3098b47eb83a2a6f6efa0ec4
julianosantannafilho <168118959+julianosantannafilho@users.noreply.github.com>
2025-06-27 00:52:01 +0000

    dapr-connection-pubsub

--- app-a/src/main/java/br/univille/app_a/controller/HomeController.java
+++ app-a/src/main/java/br/univille/app_a/controller/HomeController.java
@@ -9,6 +9,9 @@ import io.dapr.client.domain.HttpExtension;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 // dispose = desconectar os serviços
 
@@ -21,15 +24,31 @@ public class HomeController {
     @GetMapping()
     public ResponseEntity index() {
         System.out.println("Hello from App A");
+        
         try(DaprClient daprClient = new DaprClientBuilder().build()){
-
             daprClient.invokeMethod("app-b", "/api", null, HttpExtension.GET).block();
 
         } catch (Exception e){
-            //TODO: handle exceptions
+
         }
 
         return ResponseEntity.ok().body("Hello from App A");
     }
 
+    @PostMapping("/startaasync")    
+    public ResponseEntity startAAsync(){
+        System.out.println("App A started");
+        
+        try(DaprClient daprClient = new DaprClientBuilder().build()) {
+            var message = "será que funciona mesmo?";
+            
+            daprClient.publishEvent("pubsub-dapr", "topicdapr", message).block();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return ResponseEntity.ok().body("App A started");
+
+    }
 }
